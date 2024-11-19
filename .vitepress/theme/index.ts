a096f2bb6b3903c374609f52ced0eba1bd5f6bb0
liujunxiang0076 <liujunxiang0076@gmail.com>
2024-11-19 13:41:58 +0800

    主题自定义

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,15 +3,18 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import App from './components/BlogApp.vue'
 
 export default {
+  // 扩展另一个主题
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+  Layout: App
+  // Layout: () => {
+  //   return h(DefaultTheme.Layout, null, {
+  //     // https://vitepress.dev/guide/extending-default-theme#layout-slots
+  //   })
+  // },
+  // enhanceApp({ app, router, siteData }) {
+  //   // ...
+  // }
 } satisfies Theme
