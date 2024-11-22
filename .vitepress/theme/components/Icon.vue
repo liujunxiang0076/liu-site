fbd48ebb8ed3c0a28b8ba494772e0dee00f3dd42
liujunxiang0076 <liujunxiang0076@gmail.com>
2024-11-22 10:58:11 +0800

    添加图标组件

---
+++ .vitepress/theme/components/Icon.vue
@@ -0,0 +1,27 @@
+// .vitepress/theme/components/Icon.vue
+<template>
+  <i 
+    :class="['iconfont', `icon-${name}`]" 
+    :style="{ 
+      fontSize: size + 'px',
+      color
+    }"
+  ></i>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: [Number, String],
+    default: 16
+  },
+  color: {
+    type: String,
+    default: 'inherit'
+  }
+})
+</script>
