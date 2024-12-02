fdf23c051a76274dc0cf4cb2b52331ca27dea146
liujunxiang0076 <liujunxiang0076@gmail.com>
2024-12-02 16:56:53 +0800

    大更新提交

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -1,39 +1,41 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress';
 
-import { withPwa } from "@vite-pwa/vitepress";
+import { withPwa } from '@vite-pwa/vitepress';
 
 // 获取配置
-import { getThemeConfig } from "./init.mjs";
-
+import { getThemeConfig } from './init.mjs';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import path from 'path';
 // 获取主题配置
 const themeConfig = await getThemeConfig();
 
 // https://vitepress.dev/reference/site-config
 export default withPwa(
-  defineConfig({
-    // 添加 head 配置，与其他顶级配置项平级
-    head: [
-      [
-        "link",
-        {
-          rel: "stylesheet",
-          href: "https://at.alicdn.com/t/c/font_4755537_0mkagigfnwl.css",
-        },
-      ],
-    ],
-    // 引入 iconfont 脚本
-    // head: [
-    //   ['script', { src: 'https://at.alicdn.com/t/c/font_4755537_qyoxle9ja6h.js' }]
-    // ],
-    title: "liu",
-    description: "个人知识分享站",
-    // 相对于项目根目录的 markdown 文件所在的文件夹
-    srcDir: "./src",
-    cleanUrls: true,
+    defineConfig({
+        // 添加 head 配置，与其他顶级配置项平级
+        head: [
+            [
+                'link',
+                {
+                    rel: 'stylesheet',
+                    href: 'https://at.alicdn.com/t/c/font_4755537_0mkagigfnwl.css'
+                }
+            ]
+        ],
+        // 引入 iconfont 脚本
+        // head: [
+        //   ['script', { src: 'https://at.alicdn.com/t/c/font_4755537_qyoxle9ja6h.js' }]
+        // ],
+        title: 'liu',
+        description: '个人知识分享站',
+        // 相对于项目根目录的 markdown 文件所在的文件夹
+        // srcDir: './src',
+        cleanUrls: true,
 
-    themeConfig: {
-      ...themeConfig,
-      /* logo: "/logo.svg",
+        themeConfig: {
+            ...themeConfig
+            /* logo: "/logo.svg",
     siteTitle: false,
 
     // https://vitepress.dev/reference/default-theme-config
@@ -61,6 +63,49 @@ export default withPwa(
     footer: {
       copyright: "Copyright © 2022-present liu",
     },*/
-    },
-  })
+        },
+        // vite
+        vite: {
+            plugins: [
+                // 自动导入
+                AutoImport({
+                    imports: ['vue', 'vitepress'],
+                    dts: '.vitepress/auto-imports.d.ts'
+                }),
+                Components({
+                    dirs: ['.vitepress/theme/components', '.vitepress/theme/views'],
+                    extensions: ['vue', 'md'],
+                    include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+                    dts: '.vitepress/components.d.ts'
+                })
+            ],
+            resolve: {
+                // 配置路径别名
+                alias: {
+                    // eslint-disable-next-line no-undef
+                    '@': path.resolve(__dirname, './theme')
+                }
+            }
+            // css: {
+            //   preprocessorOptions: {
+            //     scss: {
+            //       silenceDeprecations: ["legacy-js-api"],
+            //     },
+            //   },
+            // },
+            // 服务器
+            // server: {
+            //   port: 9877,
+            // },
+            // 构建
+            // build: {
+            //   minify: "terser",
+            //   terserOptions: {
+            //     compress: {
+            //       pure_funcs: ["console.log"],
+            //     },
+            //   },
+            // },
+        }
+    })
 );
--- .vitepress/theme/components/Nav.vue
+++ .vitepress/theme/components/Nav.vue
@@ -1,17 +1,13 @@
 <template>
   <header class="main-header">
-    <nav class="main-nav">
+    <nav :class="['main-nav', scrollData.direction, { top: scrollData.height === 0 }]">
       <div class="nav-all">
         <!-- 导航栏左侧 -->
         <div class="left-nav">
           <div class="more-menu nav-btn" title="更多内容">
-            <i class="iconfont icon-more" style="font-size: 24px" />
+            <i class="iconfont icon-menu" />
             <div class="more-card s-card">
-              <div
-                v-for="(item, index) in theme.navMore"
-                :key="index"
-                class="more-item"
-              >
+              <div v-for="(item, index) in theme.navMore" :key="index" class="more-item">
                 <span class="more-name">{{ item.name }}</span>
                 <div class="more-list">
                   <a
@@ -28,14 +24,14 @@
               </div>
             </div>
           </div>
-          <div class="site-name">
+          <div class="site-name" @click="router.go('/')">
             {{ site.title }}
           </div>
         </div>
         <!-- 导航栏菜单 -->
         <div class="nav-center">
           <div class="site-menu">
-            <!-- <div v-for="(item, index) in theme.nav" :key="index" class="menu-item">
+            <div v-for="(item, index) in theme.nav" :key="index" class="menu-item">
               <span class="link-btn"> {{ item.text }}</span>
               <div v-if="item.items" class="link-child">
                 <span
@@ -48,11 +44,11 @@
                   {{ child.text }}
                 </span>
               </div>
-            </div> -->
+            </div>
           </div>
-          <!-- <span class="site-title" @click="smoothScrolling">
+          <span class="site-title" @click="smoothScrolling">
             {{ (frontmatter.home ? site.description : page.title) || site.description }}
-          </span> -->
+          </span>
         </div>
         <div class="right-nav">
           <!-- 开往 -->
@@ -65,36 +61,40 @@
             <i class="iconfont icon-subway"></i>
           </a>
           <!-- 随机文章 -->
-          <div class="menu-btn nav-btn" title="随机前往一篇文章" @click="">
+          <div
+            class="menu-btn nav-btn"
+            title="随机前往一篇文章"
+            @click="router.go(shufflePost(theme.postData))"
+          >
             <i class="iconfont icon-shuffle"></i>
           </div>
           <!-- 搜索 -->
-          <!-- <div
+          <div
             v-if="theme.search.enable"
             class="menu-btn nav-btn"
             title="全站搜索"
-            @click=""
+            @click="store.changeShowStatus('searchShow')"
           >
             <i class="iconfont icon-search"></i>
-          </div> -->
+          </div>
           <!-- 中控台 -->
           <div
             id="open-control"
             class="menu-btn nav-btn pc"
             title="打开中控台"
-            @click=""
+            @click="store.changeShowStatus('controlShow')"
           >
             <i class="iconfont icon-dashboard" />
           </div>
           <!-- 返回顶部 -->
-          <!-- <div
+          <div
             :class="[
               'to-top',
               'menu-btn',
               { hidden: scrollData.height === 0, long: scrollData.percentage > 90 },
             ]"
             title="返回顶部"
-            @click=""
+            @click="smoothScrolling"
           >
             <div class="to-top-btn">
               <Transition name="fade" mode="out-in">
@@ -104,54 +104,40 @@
               </Transition>
               <i class="iconfont icon-up"></i>
             </div>
-          </div> -->
+          </div>
           <!-- 移动端菜单 -->
-          <div class="menu-btn nav-btn mobile" title="打开菜单" @click="">
+          <div
+            class="menu-btn nav-btn mobile"
+            title="打开菜单"
+            @click="store.changeShowStatus('mobileMenuShow')"
+          >
             <i class="iconfont icon-toc" />
           </div>
         </div>
       </div>
     </nav>
     <!-- 移动端菜单 -->
-    <!-- <MobileMenu /> -->
+    <MobileMenu />
     <!-- 全局搜索 -->
-    <!-- <ClientOnly>
+    <ClientOnly>
       <Search v-if="theme.search.enable" />
-    </ClientOnly> -->
+    </ClientOnly>
   </header>
 </template>
 
 <script setup>
-import { useData } from "vitepress";
-import { reactive } from "vue";
-/* 内置 API 来让您访问应用程序数据 */
-const { theme, site, frontmatter, page } = useData();
-/* 测试 */
-// const theme = reactive({
-//   navMore: [
-//     {
-//       index: 1,
-//       name: "分组一",
-//       list: [
-//         { icon: "icon11", name: "name11", url: "" },
-//         { icon: "icon12", name: "name12", url: "" },
-//       ],
-//     },
-//     {
-//       index: 2,
-//       name: "分组二",
-//       list: [
-//         { icon: "icon21", name: "name21", url: "" },
-//         { icon: "icon22", name: "name22", url: "" },
-//       ],
-//     },
-//   ],
-// });
+import { storeToRefs } from "pinia";
+import { mainStore } from "@/store";
+import { smoothScrolling, shufflePost } from "@/utils/helper";
+
+const router = useRouter();
+const store = mainStore();
+const { scrollData } = storeToRefs(store);
+const { site, theme, frontmatter, page } = useData();
 </script>
 
 <style lang="scss" scoped>
 .main-header {
-  background-color: antiquewhite;
   position: relative;
   width: 100%;
   height: 60px;
@@ -164,12 +150,14 @@ const { theme, site, frontmatter, page } = useData();
     align-items: center;
     justify-content: center;
     position: fixed;
-    // top: 0;
+    top: 0;
     left: 0;
     width: 100vw;
     height: 60px;
     background-color: var(--main-card-background);
-    transition: background-color 0.3s, backdrop-filter 0.3s;
+    transition:
+      background-color 0.3s,
+      backdrop-filter 0.3s;
     &::after {
       content: "";
       position: absolute;
@@ -229,7 +217,7 @@ const { theme, site, frontmatter, page } = useData();
         .more-card {
           position: absolute;
           left: 0;
-          top: 48px;
+          top: 46px;
           opacity: 0;
           visibility: hidden;
           transform-origin: left top;
@@ -304,7 +292,7 @@ const { theme, site, frontmatter, page } = useData();
         transition: transform 0.3s;
         cursor: pointer;
         &::after {
-          content: "\e600";
+          content: "\e032";
           font-family: "iconfont";
           display: flex;
           align-items: center;
@@ -353,7 +341,9 @@ const { theme, site, frontmatter, page } = useData();
         z-index: 10;
         opacity: 0;
         transform: translateY(-50px);
-        transition: transform 0.3s, opacity 0.3s;
+        transition:
+          transform 0.3s,
+          opacity 0.3s;
         .menu-item {
           position: relative;
           padding: 0 0.4rem;
@@ -372,7 +362,9 @@ const { theme, site, frontmatter, page } = useData();
             height: 35px;
             line-height: 35px;
             border-radius: 50px;
-            transition: color 0.3s, background-color 0.3s;
+            transition:
+              color 0.3s,
+              background-color 0.3s;
           }
           .link-child {
             position: absolute;
@@ -389,7 +381,10 @@ const { theme, site, frontmatter, page } = useData();
             transform: translateY(-10px) scale(0.8);
             opacity: 0;
             visibility: hidden;
-            transition: opacity 0.3s, visibility 0.3s, transform 0.3s;
+            transition:
+              opacity 0.3s,
+              visibility 0.3s,
+              transform 0.3s;
             &::before {
               content: "";
               position: absolute;
@@ -405,7 +400,10 @@ const { theme, site, frontmatter, page } = useData();
               margin: 0 4px;
               padding: 0.6rem 0.8rem;
               white-space: nowrap;
-              transition: color 0.3s, padding 0.3s, background-color 0.3s,
+              transition:
+                color 0.3s,
+                padding 0.3s,
+                background-color 0.3s,
                 box-shadow 0.3s;
               .iconfont {
                 margin-right: 8px;
@@ -473,7 +471,9 @@ const { theme, site, frontmatter, page } = useData();
         text-overflow: ellipsis;
         overflow: hidden;
         white-space: nowrap;
-        transition: transform 0.3s, opacity 0.3s;
+        transition:
+          transform 0.3s,
+          opacity 0.3s;
         cursor: pointer;
         &::after {
           content: "返回顶部";
@@ -546,7 +546,10 @@ const { theme, site, frontmatter, page } = useData();
           height: 25px;
           border-radius: 40px;
           background-color: var(--main-font-color);
-          transition: width 0.3s, height 0.3s, background-color 0.3s;
+          transition:
+            width 0.3s,
+            height 0.3s,
+            background-color 0.3s;
           .num {
             position: absolute;
             font-size: 12px;
@@ -635,7 +638,9 @@ const { theme, site, frontmatter, page } = useData();
     .iconfont {
       font-size: 20px;
       line-height: 1;
-      transition: color 0.3s, opacity 0.3s;
+      transition:
+        color 0.3s,
+        opacity 0.3s;
     }
     &:hover {
       background-color: var(--main-color);
