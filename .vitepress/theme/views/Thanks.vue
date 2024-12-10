1fc9af7ae514da2ae72fb63fe37127b333618bd8
liujunxiang0076 <liujunxiang0076@gmail.com>
2024-12-10 09:38:07 +0800

    chore:add thanks

---
+++ .vitepress/theme/views/Thanks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+
+
+    感谢
+
+  </div>
+</template>
+<script setup lang="ts">
+import {ref, reactive} from "vue"
+
+
+
+</script>
+<style lang="scss" scoped>
+
+
+
+</style>
---
+++ src/pages/thanks.md
@@ -0,0 +1,10 @@
+---
+title: thanks
+aside: false
+---
+
+<script lang="ts" setup>
+import Thanks from "@/views/Thanks.vue"
+</script>
+
+<Thanks />
