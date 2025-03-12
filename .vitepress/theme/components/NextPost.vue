80309ab6eb11bcb75d219f292607aa8f0ae32a3a
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-03-12 13:58:18 +0800

    fix:style

--- .vitepress/theme/components/NextPost.vue
+++ .vitepress/theme/components/NextPost.vue
@@ -114,6 +114,7 @@ onBeforeUnmount(() => {
     text-overflow: ellipsis;
     display: -webkit-box;
     -webkit-line-clamp: 2;
+    line-clamp: 2;
     -webkit-box-orient: vertical;
   }
   &.fixed {
