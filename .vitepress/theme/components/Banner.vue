89a6db540e6b43d0dd859916813be6b0746b7726
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-03-31 17:08:32 +0800

    fix(Banner): 修复样式以支持行数限制
    在Banner组件中，添加了line-clamp属性以支持文本行数限制，确保在文本溢出时正确显示省略号。此更改提升了组件的可读性和用户体验。

--- .vitepress/theme/components/Banner.vue
+++ .vitepress/theme/components/Banner.vue
@@ -199,6 +199,7 @@ onBeforeUnmount(() => {
       text-overflow: ellipsis;
       display: -webkit-box;
       -webkit-line-clamp: 2;
+      line-clamp: 2;
       -webkit-box-orient: vertical;
     }
   }
